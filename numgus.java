3d59d2680237adb0c261ff14c6e3a002ec850e5b
Arish Sohail <arishsohail224@gmail.com>
2025-07-28 18:28:12 +0530

    Loop is added

--- numgus.java
+++ numgus.java
@@ -10,14 +10,22 @@ public class numgus {
         System.out.println("If your selected number is mstched to the computer selected number then will \"WIN\"");
         Random ran = new Random();
         Scanner sc = new Scanner(System.in);
-        int ui = sc.nextInt();
-        int ci = ran.nextInt(10);
-        if (ui == ci) {
-            System.out.println(":You Won The Game");
-        }
-        else{
-            System.out.println("You Lost");
+        while(true){
+            int ui = sc.nextInt();
+            int ci = ran.nextInt(10);
+            if (ui == ci) {
+                System.out.println("You Won The Game");
+            } else {
+                System.out.println("You Lost");
+            }
+            System.out.println("if you want to play again then type \"yes\" else type \"no\"");
+            String yn = sc.next();
+            if (yn.equals("no")) {
+                break;
+            }
+            else if (yn.equals("yes")) {
+                continue;
+            }
         }
     }
-    
 }
